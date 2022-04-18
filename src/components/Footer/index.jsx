266a1f55ee3fee09dbe04abdb4d8ad3e3d8d30f6
WINTHANG <haxuanthang29495@gmail.com>
2022-04-19 01:15:51 +0900

    add layout

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
+import SectionHeader from "../SectionHeader";
 import "./style.scss";
 
 export default function Footer() {
   return (
-    <section className={"footer-setion"}>
+    <section className="footer" data-scroll-section>
+      <SectionHeader title="Made in" />
 
+      <h1 className="location">All ways will success</h1>
     </section>
-  )
+  );
 }
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,24 +1,22 @@
-import React, { useState } from 'react'
-import Navbar from '../components/Navbar'
-import Header from '../components/Header'
-import Featured from '../components/Featured'
-import About from '../components/About'
-import Gallery from '../components/Gallery'
-import Footer from '../components/Footer'
-import CustomCursor from '../CustomCursor'
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import Featured from "../components/Featured";
+import About from "../components/About";
+import Gallery from "../components/Gallery";
+import Footer from "../components/Footer";
+import CustomCursor from "../CustomCursor";
 
-import '../styles/home.scss'
-
+import "../styles/home.scss";
 
 const Home = () => {
   const [preloader, setPreloader] = useState(true);
-
 
   return (
     <>
       <CustomCursor />
 
-      <div className="main-container" id='main-container'>
+      <div className="main-container" id="main-container">
         <Navbar />
         <Header />
         <Featured />
@@ -27,7 +25,7 @@ const Home = () => {
         <Footer />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
